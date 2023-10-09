6bb65c7b51a7a859eae34b4c468c0254752b8f75
heitorlimamorei <heitorlmoreira2017@outlook.com>
2023-10-09 15:00:33 +0000

    feat: add of useQuiz hook, add of lodash lib, removing strict mode

--- src/types/QuizTypes.ts
+++ src/types/QuizTypes.ts
@@ -10,3 +10,12 @@ export interface IQuestionProps {
   title: string;
   options: IQuestionOptionProps[];
 }
+
+export interface INormalizedQuestionProps {
+  // question properties used in application state
+  title: string;
+  id: string;
+  options: IQuestionOptionProps[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  answer: number;
+}
