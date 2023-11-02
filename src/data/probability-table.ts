897d2b728d712b69347a3f984e93c402405b335d
heitorlimamorei <heitorlmoreira2017@outlook.com>
2023-11-02 00:41:58 +0000

    feat: add of new probability table generator that can generate tests for any length of questions or question options

--- src/data/probability-table.ts
+++ src/data/probability-table.ts
@@ -1,40 +1,35 @@
 import _ from 'lodash';
 import questions from './questions';
 
-interface NormalizedQuestionOption {
-  opt0: number;
-  opt1: number;
-}
-
 const roundListNumbers = (collection: number[], n: number): number[] => {
   return collection.map((c) => parseFloat(c.toFixed(n)));
 };
 
-const normalizeQuestionsOptions = (): NormalizedQuestionOption[] => {
-  return questions.map((question) => {
-    return {
-      opt0: question.options[0].value,
-      opt1: question.options[1].value,
-    };
+const normalizeQuestionsOptions = (): number[][] => {
+  return questions.map((question): number[] => {
+    return question.options.map((option): number => option.value);
   });
 };
 
 const optionsList = normalizeQuestionsOptions();
 
-function tableGenerator(opts: NormalizedQuestionOption[]): number[] {
+function tableGenerator(opts: number[][]): number[] {
+  if (opts.length === 1) {
+    return opts[0].map((c) => c);
+  }
   if (opts.length === 0) {
     return [];
   }
-  if (opts.length === 1) {
-    return [opts[0].opt0, opts[0].opt1];
-  }
+
   const prevResults = tableGenerator(opts.slice(1));
   const result: number[] = [];
 
-  for (const cValue of prevResults) {
-    result.push(opts[0].opt0 * cValue);
-    result.push(opts[0].opt1 * cValue);
+  for (const cv of prevResults) {
+    opts[0].forEach((c) => {
+      result.push(c * cv);
+    });
   }
+
   return roundListNumbers(result, 6);
 }
 
